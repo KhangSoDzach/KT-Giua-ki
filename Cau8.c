90411c8f999545a95bcc84234546af2365df3728
KhangSoDzach <118435480+KhangSoDzach@users.noreply.github.com>
2022-11-20 21:36:29 +0700

    Update Cau8.c

--- Cau8.c
+++ Cau8.c
@@ -9,7 +9,7 @@ float s1(int n){
 	return sum;
 }
 int s2(int n){
-	int sum;
+	float sum;
 	int i;
 	for (i=1;i<=n;i++){
 		sum +=i;
@@ -17,10 +17,26 @@ int s2(int n){
 	return sum;
 }
 float s3(int n){
-	int i;
+	float i;
 	float sum = 0;
 	for (i=1;i<=n;i++){
 		sum += 1/i;
+	}
+	return sum;
+}
+int s4(int n){
+	int i;
+	int sum = 1;
+	for (i=1;i<=n;i++){
+		sum *=i;
+	}
+	return sum;
+}
+float s5(int n){
+	float i;
+	float sum = 0;
+	for (i=1;i<=n;i++){
+		sum += pow(-1,i)/i;
 	}
 	return sum;
 }
@@ -37,8 +53,10 @@ do{
 }	
 	
 while (n <= 0);
-	printf ("s1 =%f \n", s1(n));
-	printf ("s2 =%d \n",s2(n));
+	printf ("s1 = %f \n", s1(n));
+	printf ("s2 = %d \n",s2(n));
 	printf ("s3 = %f \n",s3(n));
+	printf ("s4 = %d \n",s4(n));
+	printf ("s5 = %f \n",s5(n));
 	return 0;
 }
